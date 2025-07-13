23f8714afad78a8c6a5a6ea9af3d2b4b789d25db
minhao23 <heminhao120@gmail.com>
2025-07-13 12:06:47 +0800

    Add persist session in supabase

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -2,6 +2,9 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
+    const supabase = createClient(supabaseUrl, supabaseKey, {  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+  },});
 
 export default supabase;
